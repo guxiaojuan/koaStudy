e1d7cbcbbe215c16bc5683f90ca47ceb4b36154f
guxiaojuan <iostream123>
2019-10-22 20:59:24 +0800

    'co'

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const Koa = require('koa/lib/application');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'hello world';
+    ctx.body = 'hello world';
+    console.log("async");
 });
 
+app.use(function* f1(next) {
+    console.log("f1 start");
+    yield  next;
+    console.log("f1 end")
+})
+
+app.use(function* f2(next) {
+    console.log("f2 start");
+    yield  next;
+    console.log("f2 end")
+})
+
+
+// http.createServer(app.callback()).listen(3001)
 app.listen(3001);
--- note.md
+++ note.md
@@ -6,6 +6,8 @@
 #### koa-compose
 > koa中间件实现洋葱模型的核心模块。
 #### co
+> co库用来自动执行generator函数    
+  co函数接受一个generator函数作为参数，返回一个promise对象
 #### delegator
 > 主要实现的是一个对象实例对其属性的代理，koa中主要为了context能够方便的访问其挂载的request，response中属性。    
   response.js和request.js都是用set，get代理的，而context.js用delegator原因是get和set可以加入自己的逻辑，但是delegator只是纯粹代理
